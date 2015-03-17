d0db24cd7caa81d2779b673bbae20aa53f4c95d6
Nathan Morin <nathanamorin@gmail.com>
2015-03-17 17:26:14 +0000

    updated to use raspicam

---
+++ build/CMakeFiles/simpletest_raspicam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/simpletest_raspicam.dir/simpletest_raspicam.cpp.o"
+  "simpletest_raspicam.pdb"
+  "simpletest_raspicam"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/simpletest_raspicam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
