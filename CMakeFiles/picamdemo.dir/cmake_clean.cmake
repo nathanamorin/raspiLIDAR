bca40b54781c36fdcb7bbeb97b34ba8a12742db9
Nathan Morin <nathanamorin@gmail.com>
2015-03-17 16:19:46 +0000

    added picam library

---
+++ CMakeFiles/picamdemo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/picamdemo.dir/picam.cpp.o"
+  "CMakeFiles/picamdemo.dir/camera.cpp.o"
+  "CMakeFiles/picamdemo.dir/cameracontrol.cpp.o"
+  "CMakeFiles/picamdemo.dir/graphics.cpp.o"
+  "picamdemo.pdb"
+  "picamdemo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/picamdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
